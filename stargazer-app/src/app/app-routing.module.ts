a71c1e09838d428a56635e1aa461d9d8a5e3d024
Jayme Fullone <jayme.fullone@yahoo.com>
2021-03-08 19:40:49 -0500

    main-style-changes

--- stargazer-app/src/app/app-routing.module.ts
+++ stargazer-app/src/app/app-routing.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { MainComponent } from './components/main/main.component';
 import { SearchComponent } from './components/search/search.component';
-
+import { StarchartComponent } from './components/starchart/starchart.component';
+import { WeatherMoonComponent } from './components/weather-moon/weather-moon.component';
 
 const routes: Routes = [
-  { path: '', component: SearchComponent },
+  { path: 'main', component: MainComponent },
+  { path: 'weather-moon', component: WeatherMoonComponent },
+  { path: 'starchart', component: StarchartComponent },
+  { path: 'about', component: AboutComponent },
+  { path: '', redirectTo: '/main', pathMatch: 'full' },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
