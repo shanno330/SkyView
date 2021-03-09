7684953fa50a08988e32f323ad7b3d51eb9644c8
Jayme Fullone <jayme.fullone@yahoo.com>
2021-03-08 22:00:32 -0500

    liveshare-changes

--- stargazer-app/src/app/components/header/header.component.ts
+++ stargazer-app/src/app/components/header/header.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  hamburgerNav: boolean = false;
+
+  toggleMenu() {
+    this.hamburgerNav = !this.hamburgerNav;
+  }
+
   constructor() { }
 
   ngOnInit(): void {
